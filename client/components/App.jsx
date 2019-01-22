bcfbb844ff3ddd7bf34627401d5df99673a5d363
alexanderwiederin <alexander.wiederin@gmail.com>
2019-01-22 16:17:19 +0100

    configure axios post(search) request

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      location: 'London, Uk',
+      vehicle_type: 'Consumer',
+      data: [],
+      metadata: {},
+    };
+
+    this.getVehicleData = this.getVehicleData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getVehicleData();
+  }
+
+  getVehicleData() {
+    const { location, vehicle_type } = this.state;
+    axios.post('https://app.joindrover.com/api/web/vehicles', { location, vehicle_type })
+      .then((response) => {
+        const { data, metadata } = response.data;
+        this.setState({ data, metadata });
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div>test</div>

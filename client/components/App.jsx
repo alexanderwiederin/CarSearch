1ab5ae852aeed131cd4e9da8b19f836532ded97b
alexanderwiederin <alexander.wiederin@gmail.com>
2019-01-24 15:57:03 +0100

    add search functionality

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 import List from './List';
+import Search from './Search';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      location: 'London, Uk',
+      location: '',
+      vehicle_make: '',
       vehicle_type: 'Consumer',
       data: [],
       metadata: {},
-      per_page: 30,
     };
 
     this.getVehicleData = this.getVehicleData.bind(this);
+    this.handleSearchInput = this.handleSearchInput.bind(this);
   }
 
   componentDidMount() {
@@ -21,8 +23,18 @@ class App extends React.Component {
   }
 
   getVehicleData() {
-    const { location, vehicle_type, per_page } = this.state;
-    axios.post('https://app.joindrover.com/api/web/vehicles', { location, vehicle_type, per_page })
+    const {
+      vehicle_type,
+      per_page,
+      location,
+      vehicle_make,
+    } = this.state;
+
+    axios.post('https://app.joindrover.com/api/web/vehicles', {
+      vehicle_type,
+      location,
+      vehicle_make,
+    })
       .then((response) => {
         const { data, metadata } = response.data;
         this.setState({ data, metadata });
@@ -30,10 +42,17 @@ class App extends React.Component {
       .catch(error => console.log(error));
   }
 
+  handleSearchInput(e, searchInput) {
+    e.preventDefault();
+    this.setState(searchInput);
+    this.getVehicleData();
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, metadata } = this.state;
     return (
       <div>
+        <Search data={metadata.aggregations} search={this.handleSearchInput} />
         <List cars={data} />
       </div>
 
---
+++ client/components/Search.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      vehicle_make: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const newState = {};
+    newState[e.target.id] = e.target.value;
+    this.setState(newState);
+  }
+
+  render() {
+    const { data, search } = this.props;
+    const { location, vehicle_make } = this.state;
+    
+    return (
+      <form>
+        Car Make
+        <select id="vehicle_make" onChange={e => this.handleChange(e)}>
+          <option key="all" value="">All</option>
+          {data ? Object.keys(data.vehicle_make)
+            .map(make => <option key={make} value={make}>{make}</option>) : null}
+        </select>
+        Location
+        <input id="location" type="text" value={location} onChange={e => this.handleChange(e)} />
+        <button type="submit" onClick={e => search(e, { location, vehicle_make })}>Submit</button>
+      </form>
+    );
+  }
+}
+
+Search.propTypes = {
+  data: PropTypes.instanceOf(Object),
+  search: PropTypes.func,
+};
+
+export default Search;
